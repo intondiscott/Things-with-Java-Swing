f36d0af7bf1aa31a0514e0d6b30600ed4637f270
intondiscott <intondiscott@gmail.com>
2022-01-09 17:30:28 -0500

    upload Java Swing Project (Part 1)

--- src/Driver.java
+++ src/Driver.java
@@ -4,15 +4,8 @@ public class Driver {
 
     public static void main(String[] args) {
        Window window = new Window();
-
        window.setTitle("App");
        window.makeWindow();
        window.createDesktop();
-
-
-
-
-
-
     }
 }
